7da959450f19ab89e3084c80be1705c464f1d9d0
Rodrigo Coacci <rcoacci@gmail.com>
2022-08-04 16:22:04 -0300

    Melhorias nos scripts de submissao de job.

--- mpi.sh
+++ mpi.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cds
 qsub <<EOF
 #!/usr/bin/env bash
 #PBS -l select=$2:ncpus=48:mpiprocs=$3
@@ -11,11 +10,11 @@ qsub <<EOF
 # load modules
 module load gcc/11.2.0 openmpi-gnu/4.1.1
 
-PROJ="${PBS_O_HOME}/parallel-tsp"
-MPIEXTRA="--mca btl_openib_allow_ib true --timestamp-output"
+PROJ="\${PBS_O_HOME}/parallel-tsp"
+MPIEXTRA="--mca btl_openib_allow_ib true --timestamp-output --oversubscribe"
 
-cd ${PBS_O_WORKDIR}
-echo "$(date) Iniciando..."
-mpirun ${MPIEXTRA} ${PROJ}/build/tsp-mpi ${PROJ}/data/$1
-echo "$(date) Fim"
+cd \${PBS_O_WORKDIR}
+echo "\$(date) Iniciando $1 com $2 nos e $3 procs"
+mpirun \${MPIEXTRA} \${PROJ}/build/tsp-mpi \${PROJ}/data/$1
+echo "\$(date) Fim"
 EOF
--- omp.sh
+++ omp.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cds
 qsub <<EOF
 #!/usr/bin/env bash
 #PBS -l select=1:ncpus=48:ompthreads=$2
@@ -11,9 +10,9 @@ qsub <<EOF
 # load modules
 module load gcc/11.2.0
 
-PROJ="${PBS_O_HOME}/parallel-tsp"
-cd ${PBS_O_WORKDIR}
-echo "$(date) Iniciando..."
-${PROJ}/build/tsp-omp ${PROJ}/data/$1
-echo "$(date) Fim"
+PROJ="\${PBS_O_HOME}/parallel-tsp"
+cd \${PBS_O_WORKDIR}
+echo "\$(date) Iniciando $1 com $2 procs"
+\${PROJ}/build/tsp-omp \${PROJ}/data/$1
+echo "\$(date) Fim"
 EOF
